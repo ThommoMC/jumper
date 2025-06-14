b3d618726f44ca7a731fbc9fa43f60c889851007
ThommoMC <111163073+ThommoMC@users.noreply.github.com.>
2025-06-14 13:57:45 +1000

    Add no BLE page and change names of some things

--- lib/main.dart
+++ lib/main.dart
@@ -2,36 +2,33 @@ import 'package:flutter/material.dart';
 import 'ble.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Jumper());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Jumper extends StatelessWidget {
+  const Jumper({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Jumper',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Vector'),
+      home: BluetoothOffPage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class DevScreenPage extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
+        title: Text("The Dev Screen"),
       ),
       body: Center(
         child: Column(
@@ -43,4 +40,25 @@ class MyHomePage extends StatelessWidget {
       ),
     );
   }
+}
+
+class BluetoothOffPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("No Bluetooth!")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.bluetooth_disabled),
+            Text("Please re-enable bluetooth")
+          ],
+        ),
+      ),
+    );
+  }
+  
 }
