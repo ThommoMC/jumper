60fbeb2d524032fe368dd308aa95d6c57a522988
ThommoMC <111163073+ThommoMC@users.noreply.github.com.>
2025-06-23 21:08:39 +1000

    Add sodium_libs package

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import flutter_blue_plus_darwin
+import sodium_libs
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
+  SodiumLibsPlugin.register(with: registry.registrar(forPlugin: "SodiumLibsPlugin"))
 }
